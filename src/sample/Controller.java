2f0dfb2ffc6da0a6a794d45afd3493a61e78fd0c
rflorian2017 <roby_florian@yahoo.com>
2019-10-06 12:03:47 +0300

    Refined the views and added alerts

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,128 @@
 package sample;
 
+import constants.Constants;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Controller {
+    public TextField txtFieldUserName;
+    public Label lblUserName;
+    public Label lblPassword;
+    public PasswordField pwdField;
+
+    public Button btnLogin;
+    public MenuItem mnuItemLogin;
+    public TabPane tabPane;
+    public Tab tabMain;
+    public Tab tabLogin;
+    public MenuItem mnuItemLogout;
+    public BorderPane borderPane;
+    public Label lblInfoToAdd;
+    public TextField txtFieldWord;
+    public TextArea txtAreaCollection;
+    public VBox hBoxWithItems;
+    public ComboBox cmbBoxItems;
+    public Button btnAdd;
+
+    public void initialize() {
+//        tabPane.getTabs().clear();
+
+        cmbBoxItems.getItems().add("one");
+        cmbBoxItems.getItems().add("two");
+        cmbBoxItems.getItems().add("three");
+    }
+
+    public void loginUser(ActionEvent actionEvent) {
+        if(txtFieldUserName.getText().equals(Constants.USERNAME)) {
+            lblUserName.setTextFill(Color.BLACK);
+            System.out.println(txtFieldUserName.getText());
+        }
+        else {
+            lblUserName.setTextFill(Color.RED);
+        }
+
+        if(pwdField.getText().equals(Constants.PASSWORD)) {
+            lblPassword.setTextFill(Color.BLACK);
+            System.out.println(pwdField.getText());
+        }
+        else {
+            lblPassword.setTextFill(Color.RED);
+        }
+
+        if(lblPassword.getTextFill().equals(Color.BLACK) && lblUserName.getTextFill().equals(Color.BLACK)) {
+            tabPane.getTabs().remove(tabLogin);
+            tabPane.getTabs().add(tabMain);
+        }
+    }
+
+    public void openLoginWindow(ActionEvent actionEvent) {
+        showClosingDialog();
+        tabPane.getTabs().add(tabLogin);
+//        for (Node node:borderPane.getChildren()) {
+//            if(node instanceof TabPane ) {
+//                for (Tab tab: ((TabPane) node).getTabs()
+//                     ) {
+//                    if(tab.getStyleClass().equals("tabCss")) {
+//                        System.out.println(tab.getText());
+//                    }
+//                }
+//            }
+//            System.out.println(node);
+//        }
+    }
+
+    public void logoutUser(ActionEvent actionEvent) {
+        tabPane.getTabs().clear();
+    }
+
+    public void addToList(KeyEvent keyEvent) {
+
+        if(keyEvent.getCode().equals(KeyCode.ENTER)) {
+            txtAreaCollection.appendText(txtFieldWord.getText() + "\n");
+            CheckBox checkBox = new CheckBox(txtFieldWord.getText());
+            hBoxWithItems.getChildren().add(checkBox);
+            txtFieldWord.clear();
+        }
+    }
+
+    public void passwordFilledIn(KeyEvent keyEvent) {
+        if(keyEvent.getCode().equals(KeyCode.ENTER)) {
+            loginUser(null);
+        }
+    }
+
+    public void addComboItemToField(ActionEvent actionEvent) {
+        txtAreaCollection.appendText(cmbBoxItems.getSelectionModel().getSelectedItem() + "\n");
+        cmbBoxItems.getSelectionModel().select(-1);
+    }
+
+    public void showClosingDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Are you sure you want to exit?");
+        alert.setHeaderText("Really, really?");
+        alert.setContentText("Ok, as you wish");
+
+        ButtonType buttonTypeYes = new ButtonType("YES");
+        ButtonType buttonTypeNo = new ButtonType("NO");
+
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == buttonTypeYes) {
+            System.out.println("Exiting!");
+        }
+        else {
+            // do nothing
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,38 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
+    Controller controller;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(getClass().getResourceAsStream("main_window.fxml"));
 
-        primaryStage.setTitle("Hello World"); // title of window
+        primaryStage.setTitle("Demo Java FX"); // title of window
         primaryStage.setScene(new Scene(root, 600, 800));
+        primaryStage.setMinHeight(800);
+        primaryStage.setMinWidth(600);
+
+        controller = fxmlLoader.getController();
+
+        //without lambda
+//        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+//                                           @Override
+//                                           public void handle(WindowEvent event) {
+//
+//                                           }
+//                                       });
+        primaryStage.setOnCloseRequest(event -> {
+            controller.showClosingDialog();
+        });
 
         primaryStage.show(); // show the actual window
     }
